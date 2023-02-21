bd3ccbaf0f3f36a14fbcfb91e07dfd7e27ff8346
Stone Comstock <comstockstone@yahoo.com>
2023-02-21 19:46:38 +0700

    Added player input for car movement

--- Assets/Driver.cs
+++ Assets/Driver.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class Driver : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField]
+    float steerSpeed = 0.1f;
 
-    // Update is called once per frame
+    [SerializeField]
+    float moveSpeed = 0.01f;
+
+    void Start() { }
+
     void Update()
     {
-        
+        // Get horizontal and veritcal key inputs, returns a value of -1 to 1. Multiply by speed variables to move and steer the car
+        // Use Time.deltaTime to have it function independently of framerate variance
+        float steerAmount = Input.GetAxis("Horizontal") * steerSpeed * Time.deltaTime;
+        float moveAmount = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
+        transform.Rotate(0, 0, -steerAmount);
+        transform.Translate(0, moveAmount, 0);
     }
 }
