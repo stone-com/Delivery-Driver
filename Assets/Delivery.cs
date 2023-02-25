ba430445cf7c1264f0d5673034d5392b7e437d8b
Stone Comstock <comstockstone@yahoo.com>
2023-02-25 23:11:48 +0700

    Change car color on pickup and dropoff

--- Assets/Delivery.cs
+++ Assets/Delivery.cs
@@ -7,7 +7,21 @@ public class Delivery : MonoBehaviour
     bool hasPackage;
 
     [SerializeField]
+    Color32 hasPackageColor = new Color32(241, 46, 0, 255);
+
+    [SerializeField]
+    Color32 noPackageColor = new Color32(1, 1, 1, 255);
+
+    [SerializeField]
     float destroyTime = 0.5f;
+
+    // Initialize variable for spriteRenderer
+    SpriteRenderer spriteRenderer;
+
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
 
     void OnCollisionEnter2D(Collision2D other)
     {
@@ -20,12 +34,14 @@ public class Delivery : MonoBehaviour
         {
             Debug.Log("Package picked up");
             hasPackage = true;
+            spriteRenderer.color = hasPackageColor;
             Destroy(other.gameObject, destroyTime);
         }
         if (other.tag == "Customer" && hasPackage)
         {
             Debug.Log("Package delivered to customer!");
             hasPackage = false;
+            spriteRenderer.color = noPackageColor;
         }
     }
 }
